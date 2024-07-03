e19713602f3e821982739e1ddbac4f5babfcce53
omarcospo <marcos.felipe@usp.br>
2024-07-03 11:50:07 -0300

    fix not escaping space in string format

--- programs/yt-dlp.nix
+++ programs/yt-dlp.nix
@@ -12,7 +12,7 @@
       embed-subs = true;
       embed-thumbnail = true;
       merge-output-format = "mkv";
-      output = "[%(uploader)s]_%(title)s.%(ext)s";
+      output = "'(%(uploader)s) %(title)s.%(ext)s'";
       S = "res:1920";
     };
   };
