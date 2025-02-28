06a8fa06f53670f5627baf5c4355ded3687a1700
omarcospo <marcos.felipe@usp.br>
2025-02-27 23:35:55 -0300

    fix vscode

--- programs/vscodium.nix
+++ programs/vscodium.nix
@@ -6,36 +6,38 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      ms-python.python
-      ms-toolsai.jupyter
-      esbenp.prettier-vscode
-      asvetliakov.vscode-neovim
-      piousdeer.adwaita-theme
-    ];
-    userSettings = {
-      # Font
-      "editor.fontFamily" = "'IosevkaTerm Nerd Font'";
-      "editor.fontLigatures" = true;
-      "editor.fontSize" = 17;
-      # Window
-      "window.title" = "''\${rootName}";
-      "window.titleBarStyle" = "custom";
-      "workbench.sideBar.location" = "right";
-      "editor.minimap.enabled" = false;
-      "breadcrumbs.enabled" = false;
-      "notebook.lineNumbers" = "on";
-      # Security
-      "security.workspace.trust.banner" = "never";
-      "security.workspace.trust.enabled" = false;
-      "security.workspace.trust.untrustedFiles" = "open";
-      # Theming
-      "workbench.colorTheme" = "Adwaita Dark";
-      "workbench.productIconTheme" = "adwaita";
-      # Language
-      "python.defaultInterpreterPath" = "~/.local/python/bin/python";
-      "extensions.experimental.affinity" = {
-        "asvetliakov.vscode-neovim" = 1;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        ms-python.python
+        ms-toolsai.jupyter
+        esbenp.prettier-vscode
+        asvetliakov.vscode-neovim
+        piousdeer.adwaita-theme
+      ];
+      userSettings = {
+        # Font
+        "editor.fontFamily" = "'IosevkaTerm Nerd Font'";
+        "editor.fontLigatures" = true;
+        "editor.fontSize" = 17;
+        # Window
+        "window.title" = "''\${rootName}";
+        "window.titleBarStyle" = "custom";
+        "workbench.sideBar.location" = "right";
+        "editor.minimap.enabled" = false;
+        "breadcrumbs.enabled" = false;
+        "notebook.lineNumbers" = "on";
+        # Security
+        "security.workspace.trust.banner" = "never";
+        "security.workspace.trust.enabled" = false;
+        "security.workspace.trust.untrustedFiles" = "open";
+        # Theming
+        "workbench.colorTheme" = "Adwaita Dark";
+        "workbench.productIconTheme" = "adwaita";
+        # Language
+        "python.defaultInterpreterPath" = "~/.local/python/bin/python";
+        "extensions.experimental.affinity" = {
+          "asvetliakov.vscode-neovim" = 1;
+        };
       };
     };
   };
